dca37b439bac07f8768d72f57ab7780b75308ffd
Tristan Carranante <tristan@carranante.name>
2014-03-04 04:48:27 +0100

    refactoring

--- comiconverter/functions.py
+++ comiconverter/functions.py
@@ -18,6 +18,9 @@ extentionByFormat = {
 def filenameFromPath(path):
     return os.path.splitext(os.path.basename(path))[0]
 
+def getExtention(fileName):
+    return os.path.splitext(f)[1].lower()
+
 def getFileList(path):
     fileList = []
 
@@ -35,60 +38,120 @@ def unpackArchive(path):
     patoolib.extract_archive(path,outdir=tmpDir)
     return tmpDir
 
-def convertArchive(path="./",imageFormat="JPEG", resize=None):
-    workingDirectoryList = []
+def convertFile(origin,format,resize):
+    try:
+        if getExtention(origin) in extentionByFormat.values():
+            im = Image.open(filePath).convert("RGB")
 
-    #os.fchdir()
-    for archivePath in getFileList(path):
-        try:
-            unpackedPath = unpackArchive(archivePath)
-            workingDirectoryList.append((filenameFromPath(archivePath),unpackedPath))
-        except Exception as err:
-            print('%s : %s' % ('Error while unpacking archives',err))
+            if resize[0]:
+                im.thumbnail((int(resize[0]),int(resize[1])), Image.ANTIALIAS)
+
+            tmpFile = BytesIO()
+            im.save(tmpFile, imageFormat, quality=85)
+            tmpFile.seek(0)
+
+            return tmpFile
+    except Exception as e:
+        print("---- error occured while converting: %s" % origin)
+        return None
 
 
-    for workingDirInfo in workingDirectoryList:
-        name = workingDirInfo[0]
-        workingDir = workingDirInfo[1]
+def convertArchive(workingDir, destFile, imageFormat="JPEG", resize=None):
+    print("--- converting to %s %s: %s" % (imageFormat, resize, destFile)
 
-        with tarfile.open('%s/%s-rev.tar' % (os.path.dirname(archivePath),name), "w") as tarFile:
-            files = os.listdir(workingDir)
-            files.sort()
-            for f in files:
-                filePath = '%s/%s' % (workingDir,f)
-                
-                extention = os.path.splitext(f)[1]
-                extention = extention.lower()
+    with tarfile.open(destFile, "w") as tarFile:
+        for dirName, subdirList, fileList in os.walk(path):
+            for fileName in fileList:
 
-                if not os.path.isdir(filePath) and extention in extentionByFormat.values():
-                    futurFileName = '%s%s' % (os.path.splitext(f)[0],extentionByFormat[imageFormat])
+                filePath = "%s%s" % (dirName,fileName)
+                futurFileName = fileName.replace(workingDir,"")
 
-                    #convert and resize
-                    im = Image.open(filePath).convert("RGB")
+                print("---- converting %s" % filePath)
+                print("---- to %s" % futurFileName)
 
-                    if resize[0]:
-                        im.thumbnail((int(resize[0]),int(resize[1])), Image.ANTIALIAS)
+                tmpFile = convertFile(filePath,imageFormat,resize)
 
-                    #put in tmpfile
-                    tmpFile = BytesIO()
-                    im.save(tmpFile, imageFormat, quality=85)
-                    tmpFile.seek(0)
-
-                    #put in tar
+                if tmpFile:
                     info = tarfile.TarInfo(futurFileName)
                     info.size = len(tmpFile.getbuffer())
-                    #info.type = "WEBP"
                     tarFile.addfile(info,tmpFile)
                     tmpFile.close()
-                    print("file %s compressed" % futurFileName)
-                else:
-                    print("file NOT %s compressed" % name)
+                    print("---- %s converted" % futurFileName)
 
-        shutil.rmtree(workingDir)
 
-        #bz2
-        '''
-        with open('%s.tar' % name, 'rb') as input:
-            with bz2.BZ2File('%s.bz2' % name, 'wb', compresslevel=9) as output:
-                copyfileobj(input, output)
-        '''
+
+def launch(path="./",imageFormat="JPEG", resize=None):
+
+    for dirName, subdirList, fileList in os.walk(path):
+        print("working in: %s" % dirName)
+
+        for fileName in fileList:
+            print("-- Unpacking: %s" % fileName)
+            filePath = "%s%s" % (dirName,fileName)
+            unpackedArchive = unpackArchive(filePath)
+
+            newTarFileName = "%s.tar" % os.path.splitext(filePath)
+            convertArchive(unpackedArchive,newTarFileName, imageFormat, resize=None)
+            shutil.rmtree(unpackedArchive)
+            print("-- Finished: %s" % newTarFileName)
+
+
+
+
+# def convertArchive(path="./",imageFormat="JPEG", resize=None):
+#     workingDirectoryList = []
+
+#     #os.fchdir()
+#     for archivePath in getFileList(path):
+#         try:
+#             unpackedPath = unpackArchive(archivePath)
+#             workingDirectoryList.append((filenameFromPath(archivePath),unpackedPath))
+#         except Exception as err:
+#             print('%s : %s' % ('Error while unpacking archives',err))
+
+
+#     for workingDirInfo in workingDirectoryList:
+#         name = workingDirInfo[0]
+#         workingDir = workingDirInfo[1]
+
+#         with tarfile.open('%s/%s-rev.tar' % (os.path.dirname(archivePath),name), "w") as tarFile:
+#             files = os.listdir(workingDir)
+#             files.sort()
+#             for f in files:
+#                 filePath = '%s/%s' % (workingDir,f)
+                
+#                 extention = os.path.splitext(f)[1]
+#                 extention = extention.lower()
+
+#                 if not os.path.isdir(filePath) and extention in extentionByFormat.values():
+#                     futurFileName = '%s%s' % (os.path.splitext(f)[0],extentionByFormat[imageFormat])
+
+#                     #convert and resize
+#                     im = Image.open(filePath).convert("RGB")
+
+#                     if resize[0]:
+#                         im.thumbnail((int(resize[0]),int(resize[1])), Image.ANTIALIAS)
+
+#                     #put in tmpfile
+#                     tmpFile = BytesIO()
+#                     im.save(tmpFile, imageFormat, quality=85)
+#                     tmpFile.seek(0)
+
+#                     #put in tar
+#                     info = tarfile.TarInfo(futurFileName)
+#                     info.size = len(tmpFile.getbuffer())
+#                     #info.type = "WEBP"
+#                     tarFile.addfile(info,tmpFile)
+#                     tmpFile.close()
+#                     print("file %s compressed" % futurFileName)
+#                 else:
+#                     print("file NOT %s compressed" % name)
+
+#         shutil.rmtree(workingDir)
+
+#         #bz2
+#         '''
+#         with open('%s.tar' % name, 'rb') as input:
+#             with bz2.BZ2File('%s.bz2' % name, 'wb', compresslevel=9) as output:
+#                 copyfileobj(input, output)
+#         '''
